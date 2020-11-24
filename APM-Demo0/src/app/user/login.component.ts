71dec56fd617202c7ff911a9821d6c512f799f1a
Nancy Chan <Chan-N@iehp.org>
2020-11-23 17:09:57 -0800

    "Strongly Typing Actions with Action Creators" Homework

--- APM-Demo0/src/app/user/login.component.ts
+++ APM-Demo0/src/app/user/login.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { State } from '../state/app.state';
 
 import { AuthService } from './auth.service';
+
+/* NgRx */
+import { Store } from '@ngrx/store';
+import { State } from '../state/app.state';
 import { getMaskUserName } from './state/user.reducer';
+import * as UserActions from './state/user.actions';
 
 @Component({
   templateUrl: './login.component.html',
@@ -34,7 +37,7 @@ export class LoginComponent implements OnInit {
   }
 
   checkChanged(): void {
-    this.store.dispatch({ type: '[User] Mask User Name' });
+    this.store.dispatch(UserActions.maskUserName());
   }
 
   login(loginForm: NgForm): void {
